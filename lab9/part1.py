928b79613886269948b8bf64ba2983b64ca2f473
annatrtl <ustas5172@gmail.com>
2026-03-20 11:58:13 -0700

    further edits idk

--- lab9/part1.py
+++ lab9/part1.py
@@ -8,46 +8,67 @@ vo = np.array([4, 4, 3.96, 3.88, 3.88, 3.84, 3.84]) #volts
 vc = np.array([4, 3.80, 3.36, 2.08, 1.20, 600e-3, 640e-3]) #volts
 ps = np.array([0, -0.78, -0.8, -0.6, -0.42, -232e-3, -252e-3]) #msec
 
-
-
 # calc
 ang_freq = 2*np.pi*freq
-log_ang_freq = np.log(ang_freq)
-ps_angle = ps*freq*(1) 
+ps_angle = ps*(0.001)*freq*(360) 
+vc_over_vo = vc/vo
 
-log_vc_over_vo = np.log(vc/vo)
+# vc/vo function fit
+def v_ratio_func(x, a, t):
+    return (a / ((1 + (x**2)*(t**2)) ** 0.5))
 
-# low-pass
-def low_pass(x, a, t):
-    return a / ((1 + (x**2)*(t**2)) ** 0.5)
+# phase shift function fit
+def ps_func(x, t):
+    return np.arctan(-1*x*t)
 
-# vc/vo vs ang_freq
+# plot 1: vc/vo vs ang_freq plot setup
 fig = plt.figure()
-plt.title('title')
+plt.title('Vc/Vo as a function of Angular Frequency (Log-Log)')
 plt.xlabel('Log Scale Angular Frequency [rad/s]')
-plt.ylabel('Log Scale Vo/Vc')
-plt.errorbar(log_ang_freq, log_vc_over_vo, fmt='.')
+plt.ylabel('Log Scale Vc/Vo')
+plt.errorbar(np.log(ang_freq), np.log(vc_over_vo), fmt='.')
+r_guesses = [1, 1e-3]
 
-# fit
-guesses = [-2, 1e-3]
+# plot 1
+popt_r, pcov_r = curve_fit(v_ratio_func, ang_freq, vc_over_vo, p0=r_guesses)
+plt.plot(np.log(ang_freq), np.log(v_ratio_func(ang_freq, *popt_r)), '-r', label=f"Fitted function")
 
-popt, pcov = curve_fit(low_pass, log_ang_freq, log_vc_over_vo, p0=guesses)
-plt.plot(log_ang_freq, low_pass(log_ang_freq, *popt), '-r', label=f"idk")
-
+# plot 1: amplitude & time constant values
+a_err = np.sqrt(np.diag(pcov_r))[0]
+r_t_err = np.sqrt(np.diag(pcov_r))[1]
+text1=f"The amplitude constant: {popt_r[0]:.9f} +/- {a_err:.9f} volts. The time constant: {popt_r[1]:.9f} +/- {r_t_err:.9f} sec."
+fig = fig.text(0.5, 0.02, text1, wrap=True, horizontalalignment='center')
 plt.legend()
 
+# plot 2: ps_ang vs ang_freq plot setup
+fig = plt.figure()
+plt.title('Phase Shift as a function of Log Scale Angular Frequency')
+plt.xlabel('Log Scale Angular Frequency [rad/s]')
+plt.ylabel('Phase Shift [degrees]')
+plt.errorbar(np.log(ang_freq[:-1]), ps_angle[:-1], fmt='.')
+ps_guesses = [1e-3]
+
+# plot 2
+popt_ps, pcov_ps = curve_fit(ps_func, ang_freq[:-1], ps_angle[:-1], p0=ps_guesses, maxfev=999999999)
+plt.plot(np.log(ang_freq[:-1]), ps_func(ang_freq[:-1], *popt_ps), '-r', label=f"Fitted function")
+print(ps_angle)
+print(ps_func(ang_freq[:-1], *popt_ps))
+
+# plot 2: time constant value
+ps_t_err = np.sqrt(np.diag(pcov_ps))[0]
+text2=f"The time constant: {popt_ps[0]:.9f} +/- {ps_t_err:.9f} sec."
+fig = fig.text(0.5, 0.02, text2, wrap=True, horizontalalignment='center')
+plt.legend()
 
 # amplitude & time constant 
-a_err = np.sqrt(np.diag(pcov))[0]
-t_err = np.sqrt(np.diag(pcov))[1]
+print(f"The amplitude constant: {popt_r[0]:.9f} +/- {a_err:.9f} volts")
+print(f"The time constant: {popt_r[1]:.9f} +/- {r_t_err:.9f} sec")
 
-print(f"The amplitude constant: {popt[0]:.9f} +/- {a_err:.9f} volts")
-print(f"The time constant: {popt[1]:.9f} +/- {t_err:.9f} sec")
-
-# acceptance test
-t_acc_err = 1e-3*np.sqrt((1000/10)**2 + ((1e-6/(0.05*1e-6))**2))
-print(f"Values agree for time constant? They agree if A - B ({abs(popt[1] - 1e-3):.3f}) < 2(aA + aB) ({2*((t_err**2) + (t_acc_err)):.3f})")
-print(f"Values agree time constant? {abs(popt[1] - 1e-3) < 2*((t_err**2) + (t_acc_err**2))}")
+# agreement test
+t_acc_err = 1e-3*np.sqrt((10/1000)**2 + ((1e-6*0.05/(1e-6))**2))
+print(t_acc_err)
+print(f"Values agree for time constant? They agree if A - B ({abs(popt_r[1] - 1e-3):.9f}) < 2(aA + aB) ({2*((r_t_err**2) + (t_acc_err**2)):.9f})")
+print(f"Values agree time constant? {abs(popt_r[1] - 1e-3) < 2*((r_t_err**2) + (t_acc_err**2))}")
 
 plt.show()
 
