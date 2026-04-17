143bfb815089dff6396804805dcd973ea669c1b2
annatrtl <ustas5172@gmail.com>
2026-04-16 19:09:10 -0700

    initial state for 12, most code completed already

---
+++ lab12/part2.py
@@ -0,0 +1,57 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.stats import linregress
+import pandas as pd
+
+# data
+file = './t7-cut.csv'
+df = pd.read_csv(file)
+voltage = df['v'].to_numpy() #mV
+bz = df['bz'].to_numpy() #microT
+time = df['time'].to_numpy() #s
+
+# calc
+voltage = voltage * 1e-3
+bz = bz * 1e-6
+dBdt = [(bz[i+1]-bz[i-1])/(time[i+1]-time[i-1]) for i in range(1, len(bz)-1)]
+dBdt = np.insert(dBdt, 0, bz[1]-bz[0])/(time[1]-time[0])
+dBdt = np.append(dBdt, bz[-1]-bz[-2])/(time[-1]-time[-2])
+
+# plot setup
+fig = plt.figure()
+plt.title('')
+plt.xlabel('')
+plt.ylabel('')
+
+coef = np.polyfit(dBdt, voltage, 1)
+lin_func = np.poly1d(coef)
+yfit = lin_func(dBdt)
+
+result = linregress(dBdt, voltage)
+
+plt.errorbar(dBdt, voltage, fmt=".")
+plt.plot(dBdt, yfit, label=f"y = ({coef[0]:.9f} +/- {result.stderr:.9f})x + ({coef[1]:.9f} +/- {result.intercept_stderr:.9f})")
+plt.legend()
+
+# residuals plot
+fig = plt.figure()
+plt.title('')
+plt.xlabel('')
+plt.ylabel('')
+
+plt.scatter(dBdt, yfit - voltage)
+
+# predicted slope
+n = 21
+d = 2.3 * 1e-2 #m
+a = 2 * np.pi * (d/2)
+pr_slope = -1 * n * a
+pr_slope_err = pr_slope * 0.05e-3
+print(pr_slope, pr_slope_err)
+print(coef[0], result.stderr)
+
+print(f"Values agree? They agree if A - B ({abs(pr_slope - coef[0]):.9f}) < 2(err_a^2 + err_b^2) ({2*((pr_slope_err**2) + (result.stderr**2)):.9f})")
+print(f"Values agree? {abs(pr_slope - coef[0]) < 2*((pr_slope_err**2) + (result.stderr**2))}")
+
+plt.show()
+
